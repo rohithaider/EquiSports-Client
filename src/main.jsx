9c1aa7a0fa76f2ba2500c622a8e02b3b50f190e6
rohithaider <rohithaider@gmail.com>
2024-12-08 22:41:26 +0600

    removing protected route from terms

--- src/main.jsx
+++ src/main.jsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
       },
       {
         path:"terms",
-        element:<ProtectedRoute><Terms/></ProtectedRoute>
+        element:<Terms/>
       },
       {
         path:"allSports",
